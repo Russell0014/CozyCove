3854d76c31cfc1ad20490a5d4cf96b5708044f7d
Russell <russells0014@gmail.com>
2024-11-01 21:09:27 +1100

    refactor listing model to use aggregation for filtered queries and include image URLs

--- server/src/models/listing.js
+++ server/src/models/listing.js
@@ -4,22 +4,26 @@ class Listing {
   static async findByFilters(filters) {
     const db = getDB();
     const query = {};
-    
+
     if (filters.location) query["address.market"] = filters.location;
     if (filters.propertyType) query.property_type = filters.propertyType;
     if (filters.bedrooms) query.bedrooms = parseInt(filters.bedrooms);
 
     return await db.collection('listingsAndReviews')
-      .find(query)
-      .project({
-        name: 1,
-        summary: 1,
-        price: 1,
-        "review_scores.review_scores_rating": 1,
-        "address.market": 1
-      })
-      .limit(10)
+      .aggregate([
+        { $match: query },
+        { $sample: { size: 10 } },
+        { $project: {
+          name: 1,
+          summary: 1, 
+          price: 1,
+          "review_scores.review_scores_rating": 1,
+          "address.market": 1,
+          "images.picture_url": 1
+        }}
+      ])
       .toArray();
+    
   }
 
   static async findById(id) {
@@ -32,7 +36,8 @@ class Listing {
           summary: 1,
           price: 1,
           "review_scores.review_scores_rating": 1,
-          "address.market": 1
+          "address.market": 1,
+          "images.picture_url": 1
         }
       }
     );
