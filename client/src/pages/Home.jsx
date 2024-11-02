94b6be8dcb069cdaaf0d51672f99c779dc9a2680
Russell <russells0014@gmail.com>
2024-11-02 15:20:41 +1100

    refactor Home component layout for improved styling and alignment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,8 @@ import Search from "../components/Search"
 
 function Home() {
     return (
-        <div className="text-center">
-            <h1><span className=" font-bold text-red-500">Discover your next adventure.</span> Book unique homes and immerse yourself in the city like a local.</h1>
+        <div className="container mx-auto text-left">
+            <h1 className="m-10 text-3xl"><span className=" font-bold text-red-500">Discover your next adventure.</span> Book unique homes and immerse yourself in the city like a local.</h1>
             <Search />
         </div>
     )
