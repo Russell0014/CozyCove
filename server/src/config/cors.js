96e690f82ca9f6efa28e52acadda44586869c116
RussellS0014 <Russells0014@gmail.com>
2024-12-30 22:09:38 +1100

    refactor CORS configuration to remove default localhost URL

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
